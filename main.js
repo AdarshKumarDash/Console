9b9b989d2c3f569ddbd46d14a5e43fb451566613
AdarshKumarDash <86519736+AdarshKumarDash@users.noreply.github.com>
2021-09-28 20:08:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("Hi this is console screen");
+
+var first_name = "Adarsh Kumar ";
+console.log(first_name);
+
+var last_name = "Dash";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name)
+console.log(full_name);
+
+var s1 = "I am learning java script";
+console.log(s1);
+
+var s2 = s1.replace("learning", "studying");
+console.log(s2);
+
+console.log(s2.toUpperCase())
+
+var s3 = "I HAVE EXAM TODAY";
+console.log(s3.toLowerCase());
+console.log(s3.length);
+
+var n1 = Math.sqrt(20);
+console.log(n1);
+
+var n2 = Math.max(2,4);
+console.log(n2);
+
+var n3 = Math.min(2,4);
+console.log(n3);
+
+var n4 = Math.pow(4,6);
+console.log(n4);
